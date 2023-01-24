9e91e211100e4b3542df14ae7bee7ec6f3a0805e
Cristian Corrales <cristiandavid.corrales@gmail.com>
2023-01-23 22:04:33 -0500

    Primero punto de corte del curso

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Template from './templates/Template.js';
 console.log('hola');
 
 (async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
+    const main = null || document.getElementById('main');
+    main.innerHTML = await Template();
 })();
